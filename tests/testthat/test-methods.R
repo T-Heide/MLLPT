e8dd2c071f228f2f7d2507fa3fa891f8a56e701c
Timon Heide <timon.heide@icr.ac.uk>
2020-12-03 15:56:05 +0000

    Updated tests.

--- tests/testthat/test-methods.R
+++ tests/testthat/test-methods.R
@@ -1,3 +1,19 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("lp assignment method works", {
+
+  load("data.rda")
+
+  lp_adding_results =
+    expect_type({
+      add_lowpass_sampled(
+        tree = tree,
+        phydata = phy_data,
+        sample_data = head(lp_data, 2),
+        return_details = TRUE
+      )
+    }, "list")
+
+  expect_s3_class(plot_lp_loglik(lp_adding_results), "ggplot")
+  expect_s3_class(plot_lp_loglik_edge(lp_adding_results), "ggplot")
+  expect_s3_class(plot_sample_data(lp_adding_results), "ggplot")
+
 })
