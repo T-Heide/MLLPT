8c91449467cf30677d6f61a01e5a6d7e31972dbe
Timon Heide <timon.heide@icr.ac.uk>
2020-12-16 11:06:37 +0000

    Example LP sample dataset.

---
+++ R/datasets.R
@@ -0,0 +1,12 @@
+#' Simulated low-pass WGS dataset for MLLPT
+#'
+#' A list containing sample tree data (phylo & phyDat object) constructed
+#' from deep WGS samples and a list of two low-pass WGS samples.
+#'
+#' @format A list containing:
+#' \describe{
+#'   \item{tree}{Phylogentic tree constructed from deep WGS data.}
+#'   \item{phydata}{PhyDat object used for the tree construction (includes id attribute).}
+#'   \item{samples}{Low-pass WGS data of two samples (EPICC_C518_C1_G1_L1 and EPICC_C518_D1_G1_L1).}
+#' }
+"example_lp_data"
