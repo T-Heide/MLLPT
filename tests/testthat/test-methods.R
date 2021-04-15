eb4e1adf21e37a8346f3843a896a699152f82385
Timon Heide <timon.heide@icr.ac.uk>
2021-04-15 17:58:12 +0100

    Updated documentation and tests.

--- tests/testthat/test-methods.R
+++ tests/testthat/test-methods.R
@@ -12,7 +12,6 @@ test_that("lp assignment method works", {
       )
     }, "list")
 
-
   lp_adding_results_plus_loss =
     expect_type({
       add_lowpass_sampled(
@@ -20,10 +19,9 @@ test_that("lp assignment method works", {
         phydata = phy_data,
         sample_data = head(lp_data, 2),
         return_details = TRUE,
-        max_loss_frac = 0.1
+        max_loss_frac = 0.5
       )
     }, "list")
-
 
   lp_adding_results_no_optim =
     expect_type({
@@ -36,16 +34,65 @@ test_that("lp assignment method works", {
       )
     }, "list")
 
+
   lp_adding_results_no_details =
-    expect_type({
+    expect_s3_class({
       add_lowpass_sampled(
         tree = tree,
         phydata = phy_data,
         sample_data = head(lp_data, 2),
         return_details = FALSE
       )
+    }, "phylo")
+
+
+  # alternative column names
+  lp_data_alt = lp_data
+  for (i in seq_along(lp_data_alt)) {
+    colnames(lp_data_alt[[i]]) = c("clone","alt","dp","id","ccf","vaf","cn")
+  }
+
+  lp_adding_results_alt_names =
+    expect_type({
+      add_lowpass_sampled(
+        tree = tree,
+        phydata = phy_data,
+        sample_data = head(lp_data_alt, 2),
+        return_details = TRUE,
+        max_vaf_bkgr=0.05
+      )
     }, "list")
 
+
+  # warning when using very high background rate?
+  expect_warning({
+    add_lowpass_sampled(
+      tree = tree,
+      phydata = phy_data,
+      sample_data = head(lp_data, 1),
+      return_details = TRUE,
+      max_vaf_bkgr = 0.5
+    )
+  }, "Do you really want to set a background rate of")
+
+
+  # failing when not including essential data?
+  for (drop in c("alt","depth","cn")) {
+
+    lp_data_alt = lp_data
+
+    for (i in seq_along(lp_data_alt))
+      lp_data_alt[[i]][,drop] = NULL
+
+    expect_error({
+      add_lowpass_sampled(
+        tree = tree,
+        phydata = phy_data,
+        sample_data = head(lp_data_alt, 2)
+      )
+    }, "missing")
+
+  }
 
   expect_s3_class(plot_lp_loglik(lp_adding_results), "ggplot")
   expect_s3_class(plot_lp_loglik_edge(lp_adding_results), "ggplot")
@@ -60,3 +107,4 @@ test_that("lp assignment method works", {
 
 
 })
+
